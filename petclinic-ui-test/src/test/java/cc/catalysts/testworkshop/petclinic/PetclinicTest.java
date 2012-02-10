4a14516cb1b3c3150972cbe37d19011c13c8d2b2
Klaus Lehner <klaus81@gmail.com>
2012-02-10 10:10:01 +0100

    code format

--- petclinic-ui-test/src/test/java/cc/catalysts/testworkshop/petclinic/PetclinicTest.java
+++ petclinic-ui-test/src/test/java/cc/catalysts/testworkshop/petclinic/PetclinicTest.java
@@ -2,13 +2,11 @@ package cc.catalysts.testworkshop.petclinic;
 
 import org.junit.Test;
 
-public class PetclinicTest extends UITestBase
-{
+public class PetclinicTest extends UITestBase {
 
-    @Test
-    public void loginTest()
-    {
-        driver.get("http://localhost:8080/petclinic");
-    }
+	@Test
+	public void loginTest() {
+		driver.get("http://localhost:8080/petclinic");
+	}
 
 }
