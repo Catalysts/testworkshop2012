691fdcdbc41b8fcb4780795789b4ac28efbb172f
Wolfgang Ebner <wolfgang.p.ebner@gmail.com>
2012-02-10 10:07:20 +0100

    updated url

--- petclinic-ui-test/src/test/java/cc/catalysts/testworkshop/petclinic/PetclinicTest.java
+++ petclinic-ui-test/src/test/java/cc/catalysts/testworkshop/petclinic/PetclinicTest.java
@@ -1,11 +1,14 @@
 package cc.catalysts.testworkshop.petclinic;
+
 import org.junit.Test;
 
-public class PetclinicTest extends UITestBase {
+public class PetclinicTest extends UITestBase
+{
 
-	@Test
-	public void loginTest() {
-		driver.get("http://localhost:8080");
-	}
+    @Test
+    public void loginTest()
+    {
+        driver.get("http://localhost:8080/petclinic");
+    }
 
 }
